05960532b2e92599a05e6ffd090c5bd2f39d4bc8
Roberto Berto <roberto.berto@gmail.com>
2020-08-02 14:11:54 -0300

    fixed small ntp servers module issues with var valiations

--- vycontrol/ntp/views.py
+++ vycontrol/ntp/views.py
@@ -8,13 +8,25 @@ from django.contrib.auth.decorators import login_required
 
 import vyos
 import vycontrol_vyos_api as vapi
+import perms
 
 @login_required
 def index(request):
     all_instances = vyos.instance_getall()
     hostname_default = vyos.get_hostname_prefered(request)
+    is_superuser = perms.get_is_superuser(request.user)
+
     ntp_srv = vapi.get_ntp(hostname_default)
-    context = {'instances': all_instances,
-               'hostname_default': hostname_default,
-               'ntp_servers': ntp_srv.data['server']}
+    ntp_servers = {}
+    if ntp_srv.success:
+        if ntp_srv.data['server'] != None:
+            ntp_servers = ntp_srv.data['server']
+
+    context = {
+        'instances':                                all_instances,
+        'hostname_default':                         hostname_default,
+        'ntp_servers' :                             ntp_servers,
+        'is_superuser' :                            is_superuser,
+    }
+
     return render(request, 'ntp/list.html', context)
