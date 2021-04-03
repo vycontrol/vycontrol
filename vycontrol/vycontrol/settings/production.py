e13c587209498f195560d25d0cc327ab79171900
Roberto Berto <roberto.berto@gmail.com>
2021-04-03 01:57:41 -0300

    amazon ses instead of sendergrid example

--- vycontrol/vycontrol/settings/production.py
+++ vycontrol/vycontrol/settings/production.py
@@ -12,7 +12,7 @@ CACHES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'user'
 EMAIL_HOST_PASSWORD = 'password'
